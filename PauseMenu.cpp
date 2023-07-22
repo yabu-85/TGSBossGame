380169de98740f8a9c6c04ac7e3f49f4efab4e80
海斗 矢吹 <yabu-85@outlook.jp>
2023-07-22 15:54:33 +0900

    モデル整理

--- PauseMenu.cpp
+++ PauseMenu.cpp
@@ -13,23 +13,19 @@ PauseMenu::~PauseMenu()
 void PauseMenu::Initialize()
 {
 	//モデルデータのロード
-	hModel_[0] = Model::Load("hako.fbx");
+	hModel_[0] = Model::Load("Human.fbx");
 	assert(hModel_[0] >= 0);
+	Model::SetAnimFrame(hModel_[0], 0, 170, 1);
 
-	hModel_[1] = Model::Load("aaa.fbx");
+	hModel_[1] = Model::Load("hako.fbx");
 	assert(hModel_[1] >= 0);
-	Model::SetAnimFrame(hModel_[1], 0, 170, 1);
 
-	hModel_[2] = Model::Load("monkey.fbx");
+	hModel_[2] = Model::Load("eee.fbx");
 	assert(hModel_[2] >= 0);
+	Model::SetAnimFrame(hModel_[2], 0, 120, 1);
 
-	hModel_[3] = Model::Load("huu.fbx");
+	hModel_[3] = Model::Load("ccc.fbx");
 	assert(hModel_[3] >= 0);
-	Model::SetAnimFrame(hModel_[1], 0, 170, 1);
-
-	hModel_[4] = Model::Load("ccc.fbx");
-	assert(hModel_[4] >= 0);
-	Model::SetAnimFrame(hModel_[4], 0, 120, 1);
 
 
 }
@@ -53,23 +49,18 @@ void PauseMenu::Draw()
 	Model::Draw(hModel_[1]);
 
 	Transform pos2 = transform_;
-	pos2.position_.x = 6.5f;
-	pos2.scale_.x = 2.0f;
-	pos2.scale_.y = 2.0f;
-	pos2.scale_.z = 2.0f;
+	pos2.position_.x = 15.5f;
+	pos2.position_.z = 8.0f;
+	pos2.rotate_.y = 180.0f;
 	Model::SetTransform(hModel_[2], pos2);
 	Model::Draw(hModel_[2]);
 
 	Transform pos3 = transform_;
-	pos3.position_.x = 10.0f;
+	pos3.position_.x = 5.0f;
+	pos3.position_.y = 3.0f;
+	pos3.position_.z = 7.0f;
 	Model::SetTransform(hModel_[3], pos3);
 	Model::Draw(hModel_[3]);
-
-	Transform pos4 = transform_;
-	pos4.position_.x = 14.5f;
-	pos4.position_.y = 3.0f;
-	Model::SetTransform(hModel_[4], pos4);
-	Model::Draw(hModel_[4]);
 
 }
 
